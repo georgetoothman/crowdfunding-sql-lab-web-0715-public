87bfddb8d7822cbe44abfd4b775d1b9bc9aa825c
George Toothman <george.toothman@flatironschool.com>
2015-08-15 13:23:16 -0400

    create, insert, select sql using group by, sum and having

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,11 @@
--- your code here
+CREATE TABLE projects (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                      title TEXT, funding_goal INTEGER, 
+                      start_date DATE, category TEXT,
+                       end_date DATE);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT, age INTEGER);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      amount INTEGER, user_id INTEGER,
+                      project_id INTEGER);
